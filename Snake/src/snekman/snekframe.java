206f8aa43a96a1ba210d8ae23d922e8c423d9580
RudranshKi <110120694+RudranshKi@users.noreply.github.com>
2022-09-03 20:29:13 +0530

    Update snekframe.java

--- Snake/src/snekman/snekframe.java
+++ Snake/src/snekman/snekframe.java
@@ -1,14 +1,14 @@
 package snekman;
 
 import java.awt.Color;
-
-import javax.swing.JFrame;
+import javax.swing.*;
 
 public class snekframe extends JFrame {
 	snekpanel sp = new snekpanel();
+	ImageIcon icon = new ImageIcon("snake.png");
 	snekframe(){
 		this.setTitle("Snek Man strikes again");
-//		this.setLocationRelativeTo(null);
+		this.setIconImage(icon.getImage());
 		this.add(sp);
 		this.setVisible(true);
 		this.setResizable(false);
